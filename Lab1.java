469267b7fe881b3ba229d01375d91f6af4df68b6
goodvice <89296971+goodv1ce@users.noreply.github.com>
2022-02-04 10:12:43 +0200

    Update and rename Main.java to Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Lab1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter 'a'");
