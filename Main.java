889ff56f40e1d5f0c78233c610a69aec1caebe66
goodvice <89296971+goodv1ce@users.noreply.github.com>
2022-02-04 10:11:11 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 'a'");
+        final int a = scan.nextInt();
+        System.out.println("Enter 'b'");
+        final int b = scan.nextInt();
+        System.out.println("Enter 'n'");
+        final int n = scan.nextInt();
+        System.out.println("Enter 'm'");
+        final int m = scan.nextInt();
+        float result = 0;
+        int c = 1;
+        if (a <= c && n >= c) {
+            System.out.println("Error");
+        } else if (a > n || b > m) {
+            System.out.println(result);
+        } else {
+            for (float i = a; i <= n; i++) {
+                for (float j = b; j <= m; j++) {
+                    result += (i * j) / (i - c);
+                }
+            }
+            System.out.println(result);
+        }
+    }
+}
